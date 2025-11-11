15809abb0179bc1b00b93cc5223105e090d0a484
Shubham Ghag <98953981+Shubhammer7@users.noreply.github.com>
2025-11-11 17:11:54 +0530

    Update modelService.js

--- src/services/modelService.js
+++ src/services/modelService.js
@@ -2,10 +2,6 @@
 
 const API_BASE_URL = "https://myfoodisgettingcold.onrender.com";
 
-
-
-// const API_KEY = '1f92844c436b3f6ea0f106be7f6fa358'; // You might still need this for direct TMDB calls in development
-
 export const findSimilarMovies = async (movieTitle) => {
   try {
     const response = await fetch(`${API_BASE_URL}/recommend`, {
